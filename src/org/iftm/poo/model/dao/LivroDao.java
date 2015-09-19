4cf6dd01b1125106bac07e3056b8761fdf312b84
Victor Hugo Molinar <victormolinar@softbox.com.br>
2015-09-18 21:15:57 -0300

    Mapeando

--- src/org/iftm/poo/model/dao/LivroDao.java
+++ src/org/iftm/poo/model/dao/LivroDao.java
@@ -53,58 +53,58 @@ public class LivroDao extends TemplateDao<Livro>{
     
 	@Override
     protected List<Livro> pesquisar(EntityManager em, Livro livro) throws Exception {
-//        StringBuilder sql = new StringBuilder("select livro from Livro as livro ");
-//        sql.append(" inner join fetch livro.categoria as categoria");
-//        sql.append(" inner join fetch livro.autor as autor");
-//        sql.append(" where 1=1 ");
-//        
-//        if(livro.getCodLivro() != null){
-//            sql.append("and livro.codLivro = :codigo ");
-//        }
-//        if(livro.getEdicao() != null && !livro.getEdicao().equals("")){
-//            sql.append("and livro.edicao like :edicao ");
-//        }
-//        if(livro.getAno() != null){
-//            sql.append("and livro.ano = :ano ");
-//        }
-//        if(livro.getNome() != null && !livro.getNome().equals("")){
-//            sql.append("and livro.nome like :nome ");
-//        }
-//        if(livro.getCategoria() != null && 
-//            livro.getCategoria().getDescricao() != null && !livro.getCategoria().getDescricao().equals("")){
-//            sql.append("and categoria.descricao like :categoria");
-//        }
-//        if(livro.getAutor() != null && 
-//            livro.getAutor().getNome() != null &&
-//            !livro.getAutor().getNome().equals("")){
-//            sql.append("and autor.nome like :autor");
-//        }
-//        
-//        Query query = em.createQuery(sql.toString());
-//        if(livro.getCodLivro() != null){
-//            query.setParameter("codigo", livro.getCodLivro());
-//        }
-//        if(livro.getEdicao() != null && !livro.getEdicao().equals("")){
-//            query.setParameter("edicao", "%" + livro.getEdicao().toLowerCase() + "%");
-//        }
-//        if(livro.getAno() != null){
-//            query.setParameter("ano", livro.getAno());
-//        }
-//        if(livro.getNome() != null && !livro.getNome().equals("")){
-//            query.setParameter("nome", "%" + livro.getNome().toLowerCase() + "%");
-//        }
-//        if(livro.getCategoria() != null && 
-//            livro.getCategoria().getDescricao() != null && !livro.getCategoria().getDescricao().equals("")){
-//            query.setParameter("categoria", "%" + livro.getCategoria().getDescricao().toLowerCase() + "%");
-//        }
-//        if(livro.getAutor() != null && 
-//            livro.getAutor().getNome() != null &&
-//            !livro.getAutor().getNome().equals("")){
-//            query.setParameter("autor", "%" + livro.getAutor().getNome().toLowerCase() + "%");
-//        }
-//        
-//        return query.getResultList();
+        StringBuilder sql = new StringBuilder("select livro from Livro as livro ");
+        sql.append(" inner join fetch livro.categoria as categoria");
+        sql.append(" inner join fetch livro.autor as autor");
+        sql.append(" where 1=1 ");
+        
+        if(livro.getCodLivro() != null){
+            sql.append("and livro.codLivro = :codigo ");
+        }
+        if(livro.getEdicao() != null && !livro.getEdicao().equals("")){
+            sql.append("and livro.edicao like :edicao ");
+        }
+        if(livro.getAno() != null){
+            sql.append("and livro.ano = :ano ");
+        }
+        if(livro.getNome() != null && !livro.getNome().equals("")){
+            sql.append("and livro.nome like :nome ");
+        }
+        if(livro.getCategoria() != null && 
+            livro.getCategoria().getDescricao() != null && !livro.getCategoria().getDescricao().equals("")){
+            sql.append("and categoria.descricao like :categoria");
+        }
+        if(livro.getAutor() != null && 
+            livro.getAutor().getNome() != null &&
+            !livro.getAutor().getNome().equals("")){
+            sql.append("and autor.nome like :autor");
+        }
+        
+        Query query = em.createQuery(sql.toString());
+        if(livro.getCodLivro() != null){
+            query.setParameter("codigo", livro.getCodLivro());
+        }
+        if(livro.getEdicao() != null && !livro.getEdicao().equals("")){
+            query.setParameter("edicao", "%" + livro.getEdicao().toLowerCase() + "%");
+        }
+        if(livro.getAno() != null){
+            query.setParameter("ano", livro.getAno());
+        }
+        if(livro.getNome() != null && !livro.getNome().equals("")){
+            query.setParameter("nome", "%" + livro.getNome().toLowerCase() + "%");
+        }
+        if(livro.getCategoria() != null && 
+            livro.getCategoria().getDescricao() != null && !livro.getCategoria().getDescricao().equals("")){
+            query.setParameter("categoria", "%" + livro.getCategoria().getDescricao().toLowerCase() + "%");
+        }
+        if(livro.getAutor() != null && 
+            livro.getAutor().getNome() != null &&
+            !livro.getAutor().getNome().equals("")){
+            query.setParameter("autor", "%" + livro.getAutor().getNome().toLowerCase() + "%");
+        }
+        
+        return query.getResultList();
     	
-    	return em.createQuery("select l from Livro l", Livro.class).getResultList();
+//    	return em.createQuery("select l from Livro l", Livro.class).getResultList();
     }
 }
